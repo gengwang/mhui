e38dcb6ef310112958e237d88a83451cdff95409
qmh684 <geng.wang@motorolasolutions.com>
2014-08-13 10:51:08 -0500

    Make default karma test to be not singleRun.

--- site/Gruntfile.js
+++ site/Gruntfile.js
@@ -348,7 +348,7 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'test/karma.conf.js',
-        singleRun: true
+        // singleRun: true
       }
     }
   });
